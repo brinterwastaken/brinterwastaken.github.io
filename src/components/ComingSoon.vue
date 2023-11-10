5a98b48f97af6264c790dce0df1bef279a51b899
Trinethr N <trinethr_n@protonmail.com>
2023-11-10 20:21:01 +0530

    coming soon component

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,149 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import comingSoon from '../assets/coming-soon.svg'
+
+const bgtext = ref('')
+const originalText = 'OLD WEBSITE HERE'
+const linktext = ref('OLD WEBSITE HERE')
+
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
+  randomString = (length: number) => Array.from(Array(length)).map(randomChar).join('')
+
+const updateBgText = () => bgtext.value = randomString(10000)
+
+const linkEffect = () => {
+  let iterations = 0
+
+  const refreshText = setInterval(() => {
+    linktext.value = linktext.value
+      .split('')
+      .map((letter, index) => {
+        if (letter == ' ') return letter
+
+        if (index < iterations) {
+          return originalText[index]
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join('')
+    iterations += 1 / 3
+    if (iterations > originalText.length) clearInterval(refreshText)
+  }, 25)
+}
+
+updateBgText();
+
+document.onmousemove = updateBgText;
+</script>
+
+<template>
+  <div class="ellipsebox">
+    <div class="ellipse" id="e1"></div>
+    <div class="ellipse" id="e2"></div>
+    <div class="ellipse" id="e3"></div>
+  </div>
+  <div class="blurbox"></div>
+  <div id="box">
+    {{ bgtext }}
+  </div>
+  <div class="comingsoon">
+    <img :src="comingSoon" alt="" />
+    <a @mouseover="linkEffect" class="link" href="https://brin.is-a.dev/old-website-2">{{
+      linktext
+    }}</a>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+
+.blurbox {
+  backdrop-filter: blur(7.5rem);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+
+#box {
+  user-select: none;
+  width: 110%;
+  height: 110%;
+  color: #ffffff;
+  word-wrap: break-word;
+  overflow: hidden;
+  font-size: 0.9rem;
+  font-family: 'Space Mono';
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  mix-blend-mode: overlay;
+  mask-image: linear-gradient(white 0%, transparent 50%);
+  -webkit-mask-image: linear-gradient(white 0%, transparent 50%);
+}
+
+.ellipsebox {
+  position: fixed;
+  top: -25vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: -5vw;
+  z-index: -2;
+}
+.ellipse {
+  border-radius: 50%;
+}
+#e1,
+#e3 {
+  height: 45vh;
+  width: 40vw;
+  z-index: -1;
+}
+#e1 {
+  background-color: #8ab9ff;
+}
+#e3 {
+  background-color: #dd95ff;
+}
+#e2 {
+  background-color: #8afff1;
+  height: 25vh;
+  width: 30vw;
+  transform: scale(140%);
+}
+.comingsoon {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  height: 100vh;
+  width: 100vw;
+  color: transparent;
+  justify-content: center;
+  align-items: center;
+}
+.comingsoon img {
+  width: 60rem;
+}
+.comingsoon .link {
+  all: unset;
+  cursor: pointer;
+  font-size: 1.5rem;
+  color: #c2eaff;
+  font-family: 'Space Mono';
+  transition: text-shadow 0.4s;
+}
+.comingsoon .link:hover {
+  text-decoration: underline;
+  text-shadow: 0 0.1rem 2rem #75b1d2;
+}
+</style>
