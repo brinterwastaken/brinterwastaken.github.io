65839176d9f6bfc1135f7ba3f70b1f29bfdfec00
Trinethr <72494265+brinterwastaken@users.noreply.github.com>
2025-03-23 00:02:04 +0530

    Revert "fixed font name"
    This reverts commit 3eaea584a1cf508740ddcbe4baf2ff70b230b766.

--- src/components/FancyHeading.vue
+++ src/components/FancyHeading.vue
@@ -19,7 +19,7 @@ const props = defineProps({ text: String, fontSize: Number, overlayBg: String })
 <style>
 svg {
   font-weight: 800;
-  font-family: 'Satoshi';
+  font-family: 'Satoshi Variable';
   width: 100%;
 }
 
